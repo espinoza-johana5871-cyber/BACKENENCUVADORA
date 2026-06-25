2c634f84080424a6f8b603c3659b91900f16d9a7
encuvadora <snayder2003ch@gmail.com>
2026-06-24 22:46:42 -0500

    con docke commit

---
+++ config/cors.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'paths'                    => ['api/*'],
+    'allowed_methods'          => ['*'],
+    'allowed_origins'          => array_filter([
+        env('FRONTEND_URL', 'http://localhost:5173'),
+        'http://localhost:5173',
+        'http://localhost:5174',
+    ]),
+    'allowed_origins_patterns' => [],
+    'allowed_headers'          => ['*'],
+    'exposed_headers'          => [],
+    'max_age'                  => 0,
+    'supports_credentials'     => false,
+];
