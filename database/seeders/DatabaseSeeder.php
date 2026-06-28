a45c71174db638a82d1f66000709236ece39287c
encuvadora <snayder2003ch@gmail.com>
2026-06-27 23:43:43 -0500

    feat: crear 3 usuarios de prueba con firstOrCreate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,12 +10,38 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'nombre' => 'Administrador',
-            'correo' => 'admin@uniincubadora.edu.ec',
-            'clave'  => Hash::make('password1234'),
-            'rol'    => 'administrador',
-            'estado' => 'activo',
-        ]);
+        $usuarios = [
+            [
+                'correo' => 'admin@uniincubadora.edu.ec',
+                'nombre' => 'Administrador',
+                'clave'  => 'admin1234',
+                'rol'    => 'administrador',
+            ],
+            [
+                'correo' => 'mentor@uniincubadora.edu.ec',
+                'nombre' => 'Carlos Mentor',
+                'clave'  => 'mentor1234',
+                'rol'    => 'mentor',
+            ],
+            [
+                'correo' => 'estudiante@uniincubadora.edu.ec',
+                'nombre' => 'Maria Emprendedora',
+                'clave'  => 'estudiante1234',
+                'rol'    => 'emprendedor',
+            ],
+        ];
+
+        foreach ($usuarios as $u) {
+            User::firstOrCreate(
+                ['correo' => $u['correo']],
+                [
+                    'nombre'         => $u['nombre'],
+                    'clave'          => Hash::make($u['clave']),
+                    'rol'            => $u['rol'],
+                    'estado'         => 'activo',
+                    'fecha_registro' => now(),
+                ]
+            );
+        }
     }
 }
